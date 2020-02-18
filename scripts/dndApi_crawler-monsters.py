97052831ed4970920ebafd6de924f95e8130a1bd
pcaly626 <pcaly626@gmail.com>
2020-02-18 08:22:43 -0600

    Removed json files

--- scripts/dndApi_crawler-monsters.py
+++ scripts/dndApi_crawler-monsters.py
@@ -15,7 +15,9 @@ for monster in data["results"]:
 extractData = ["name", "size", "type", "subtype", "armor_class", "hit_points", "speed", "strength", "dexterity",
                "constitution", "intelligence", "wisdom", "charisma", "special_abilities", "damage_resistances",
                "damage_immunities", "condition_immunities", "actions", "legendary_actions", "challenge_rating"]
-stop = False
+
+legendary_action_id = 0
+
 # steps through the alphabet
 for c in string.ascii_lowercase:
     monsterAttributes = {}
@@ -25,20 +27,18 @@ for c in string.ascii_lowercase:
         startswith = monster["index"][0]
         name = monster["index"]
         monster_data = {}
-        print(startswith)
         # index[0] is the monster name. if it doesn't start with the proper letter, it is skipped
         if startswith != c:
             continue
         for label in monster:
             if label in extractData:
+                if label == "legendary_actions":
+                    print(type(monster[label]) )
+                    legendary_action_id =  legendary_action_id + 1
+                    monster[label].append({"legendary_action_id" : legendary_action_id})
+                    print(monster[label])
                 info = monster[label]
                 monster_data[label] = info
-        # for extract in extractData:
-            # every time "extract" updates here, it carries through to cleanedResults[extract]
-            # for every entry because "extract" is a reference
-            # if "actions" not in monster:
-                # continue
-            # cleanedResults[extract] = monster[extract]
         monsterAttributes[name] = monster_data
     with open("monsters/monster_" + c + ".json", "w") as file:
         file.write(json.dumps(monsterAttributes))
